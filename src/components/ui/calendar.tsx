f2fd1c31a625a4932aeea75ffb878f1e83b23a27
Dharshanick 2004 <dharshanick02@gmail.com>
2025-12-09 04:35:55 +0000

    Fix calendar icon type error

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,13 +54,14 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
+        PreviousMonthButton: ({ className, ...props }) => (
           <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
         ),
-        IconRight: ({ className, ...props }) => (
+        NextMonthButton: ({ className, ...props }) => (
           <ChevronRight className={cn("h-4 w-4", className)} {...props} />
         ),
       }}
+      
       {...props}
     />
   )
